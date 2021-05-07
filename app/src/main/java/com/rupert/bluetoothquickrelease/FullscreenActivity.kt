1ad246108bfd2c9ade63df57d0bb8c6b3b8023f8
Rupert Bogensperger <47078678+Rupert-com@users.noreply.github.com>
2021-05-07 17:33:15 +0200

    Submit LongPress Events

--- app/src/main/java/com/rupert/bluetoothquickrelease/FullscreenActivity.kt
+++ app/src/main/java/com/rupert/bluetoothquickrelease/FullscreenActivity.kt
@@ -1,152 +1,173 @@
 package com.rupert.bluetoothquickrelease
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.annotation.SuppressLint
-import android.os.Bundle
-import android.os.Handler
-import android.view.MotionEvent
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.*
 import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.security.Permission
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-class FullscreenActivity : AppCompatActivity() {
-    private lateinit var fullscreenContent: TextView
-    private lateinit var fullscreenContentControls: LinearLayout
-    private val hideHandler = Handler()
+class FullscreenActivity() : AppCompatActivity() {
+    private lateinit var btn_Scan: Button
+    private lateinit var lv_Devices: ListView
+    private lateinit var lv_Devices_selected: ListView
 
-    @SuppressLint("InlinedApi")
-    private val hidePart2Runnable = Runnable {
-        // Delayed removal of status and navigation bar
+    private var bluetoothDevices: MutableList<BluetoothDevice> = mutableListOf()
+    private var bluetoothDevices_selected: MutableList<BluetoothDevice> = mutableListOf()
 
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-        fullscreenContent.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                        View.SYSTEM_UI_FLAG_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-    }
-    private val showPart2Runnable = Runnable {
-        // Delayed display of UI elements
-        supportActionBar?.show()
-        fullscreenContentControls.visibility = View.VISIBLE
-    }
-    private var isFullscreen: Boolean = false
-
-    private val hideRunnable = Runnable { hide() }
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private val delayHideTouchListener = View.OnTouchListener { view, motionEvent ->
-        when (motionEvent.action) {
-            MotionEvent.ACTION_DOWN -> if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS)
-            }
-            MotionEvent.ACTION_UP -> view.performClick()
-            else -> {
-            }
-        }
-        false
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_fullscreen)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        isFullscreen = true
+        btn_Scan = findViewById(R.id.buttonScan)
+        lv_Devices = findViewById<ListView>(R.id.listView)
+        lv_Devices_selected = findViewById<ListView>(R.id.listView_selected)
 
-        // Set up the user interaction to manually show or hide the system UI.
-        fullscreenContent = findViewById(R.id.fullscreen_content)
-        fullscreenContent.setOnClickListener { toggle() }
 
-        fullscreenContentControls = findViewById(R.id.fullscreen_content_controls)
+        lv_Devices_selected.setOnItemLongClickListener(AdapterView.OnItemLongClickListener() { parent, view, position, id ->
+            bluetoothDevices.add(bluetoothDevices_selected.get(position))
+            bluetoothDevices_selected.removeAt(position)
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById<Button>(R.id.dummy_button).setOnTouchListener(delayHideTouchListener)
+            renderBluetoothDevices()
+            return@OnItemLongClickListener true
+        })
+
+        lv_Devices.setOnItemLongClickListener(AdapterView.OnItemLongClickListener() { parent, view, position, id ->
+
+            bluetoothDevices_selected.add(bluetoothDevices.get(position))
+            bluetoothDevices.removeAt(position)
+
+            renderBluetoothDevices()
+            return@OnItemLongClickListener true
+        })
+// AdapterView<?> parent, View view, int position, long id
+//        lv_Devices.setOnItemClickListener(AdapterView.OnItemClickListener() { parent, view, position, id ->
+//            Toast.makeText(this, "Clicked item : $position", Toast.LENGTH_SHORT).show()
+        //      })
+        btn_Scan.setOnClickListener { scan() }
     }
 
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100)
-    }
+    private fun scan() {
+        if (!this.hasAllPermissions()) {
+            this.requestPermissions(baseContext, this);
+            btn_Scan.setText(R.string.btn_scan_retry)
+            return;
+        }
 
-    private fun toggle() {
-        if (isFullscreen) {
-            hide()
+        if (this.supportsBluetooth()) {
+            this.enableBluetooth()
+            bluetoothAdapter?.bondedDevices?.forEach { bluetoothDevice ->
+                if (!bluetoothDevices.contains(bluetoothDevice) && !bluetoothDevices_selected.contains(
+                        bluetoothDevice
+                    )
+                ) {
+                    bluetoothDevices.add(bluetoothDevice)
+                }
+            }
+            renderBluetoothDevices()
         } else {
-            show()
+            btn_Scan.setText(R.string.btn_not_supportet)
         }
     }
 
-    private fun hide() {
-        // Hide UI first
-        supportActionBar?.hide()
-        fullscreenContentControls.visibility = View.GONE
-        isFullscreen = false
+    private fun renderBluetoothDevices() {
+        val listItems = arrayOfNulls<String>(bluetoothDevices.size)
+        val listItems_selected = arrayOfNulls<String>(bluetoothDevices_selected.size)
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        hideHandler.removeCallbacks(showPart2Runnable)
-        hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+        for (i in 0 until bluetoothDevices.size) {
+            val cDevice = bluetoothDevices[i]
+            listItems[i] = cDevice.name
+        }
+
+        for (i in 0 until bluetoothDevices_selected.size) {
+            val cDevice = bluetoothDevices_selected[i]
+            listItems_selected[i] = cDevice.name
+        }
+
+        lv_Devices.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        lv_Devices_selected.adapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems_selected)
     }
 
-    private fun show() {
-        // Show the system bar
-        fullscreenContent.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        isFullscreen = true
+    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
-        // Schedule a runnable to display UI elements after a delay
-        hideHandler.removeCallbacks(hidePart2Runnable)
-        hideHandler.postDelayed(showPart2Runnable, UI_ANIMATION_DELAY.toLong())
+    fun hasAllPermissions(): Boolean =
+        PERMISSIONS().filter { cPermission -> !hasPermissionGranted(cPermission) }.size > 0
+
+    fun supportsBluetooth() = (bluetoothAdapter != null)
+
+    fun enableBluetooth() {
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        ActivityCompat.startActivityForResult(this, enableBtIntent, Intent_REQUEST_ENABLE_BT, null)
     }
 
-    /**
-     * Schedules a call to hide() in [delayMillis], canceling any
-     * previously scheduled calls.
-     */
-    private fun delayedHide(delayMillis: Int) {
-        hideHandler.removeCallbacks(hideRunnable)
-        hideHandler.postDelayed(hideRunnable, delayMillis.toLong())
+    private fun requestPermissions(baseContext: Context, activity: Activity) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (!hasPermissionGranted(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS(),
+                    Permission_ACCESS_BACKGROUND_LOCATION
+                )
+            }
+        }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            Permission_ACCESS_BACKGROUND_LOCATION -> {
+                for ((cCount, permission) in permissions.withIndex()) {
+                    if (grantResults[cCount] == PackageManager.PERMISSION_GRANTED) {
+
+                        // Permission Granted
+                    }
+                }
+            }
+        }
+    }
+
+    private fun hasPermissionGranted(pPermission: String): Boolean =
+        ContextCompat.checkSelfPermission(baseContext, pPermission) == PERMISSION_GRANTED
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            Intent_REQUEST_ENABLE_BT -> {
+                if (resultCode == RESULT_OK) {
+
+                }
+            }
+        }
+    }
+
 
     companion object {
-        /**
-         * Whether or not the system UI should be auto-hidden after
-         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
-         */
-        private const val AUTO_HIDE = true
-
-        /**
-         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
-         * user interaction before hiding the system UI.
-         */
-        private const val AUTO_HIDE_DELAY_MILLIS = 3000
-
-        /**
-         * Some older devices needs a small delay between UI widget updates
-         * and a change of the status and navigation bar.
-         */
-        private const val UI_ANIMATION_DELAY = 300
+        var Intent_REQUEST_ENABLE_BT: Int = 1
+        var Permission_ACCESS_BACKGROUND_LOCATION: Int = 2
+        fun PERMISSIONS(): Array<String> = arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
     }
 }
